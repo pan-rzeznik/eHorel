671b1a8a64bdd0a6888808b26698147c3065b4a5
pan-rzeznik <spokojny.rzeznik@gmail.com>
2019-07-31 18:20:05 +0200

    Add routing
    Add basic routing to the navbar without categories parameters

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { NewsComponent } from './news/news.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'news',
+    component: NewsComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'categories/:name',
+    component: CategoriesComponent
+  },
+  {
+    path: 'categories',
+    component: CategoriesComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
